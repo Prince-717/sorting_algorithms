f4b9f7405bfc60c1b4cdf7668eb29223d00f668a
Prince Aboagye <aboagyeprince717@gmail.com>
2023-06-27 14:26:21 -0700

    Write a function that sorts an array of integers in ascending order using the Counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,23 +1,23 @@
-#include "sort.h"
+#include "sort.pointer_h"
 
 /**
- * get_max - Get the maximum value in an array of integers.
+ * retrieve_max - Get the maximum value in an array of integers.
  * @array: An array of integers.
  * @size: The size of the array.
  *
  * Return: The maximum integer in the array.
  */
-int get_max(int *array, int size)
+int retrieve_max(int *array, int size)
 {
-	int max, i;
+	int maximum, i;
 
-	for (max = array[0], i = 1; i < size; i++)
+	for (maximum = array[0], i = 1; i < size; i++)
 	{
-		if (array[i] > max)
-			max = array[i];
+		if (array[i] > maximum)
+			maximum = array[i];
 	}
 
-	return (max);
+	return (maximum);
 }
 
 /**
@@ -30,39 +30,39 @@ int get_max(int *array, int size)
  */
 void counting_sort(int *array, size_t size)
 {
-	int *count, *sorted, max, i;
+	int *numb, *arranged, maximum, i;
 
 	if (array == NULL || size < 2)
 		return;
 
-	sorted = malloc(sizeof(int) * size);
-	if (sorted == NULL)
+	arranged = malloc(sizeof(int) * size);
+	if (arranged == NULL)
 		return;
-	max = get_max(array, size);
-	count = malloc(sizeof(int) * (max + 1));
-	if (count == NULL)
+	maximum = retrieve_max(array, size);
+	numb = malloc(sizeof(int) * (maximum + 1));
+	if (numb == NULL)
 	{
-		free(sorted);
+		free(arranged);
 		return;
 	}
 
-	for (i = 0; i < (max + 1); i++)
-		count[i] = 0;
+	for (i = 0; i < (maximum + 1); i++)
+		numb[i] = 0;
 	for (i = 0; i < (int)size; i++)
-		count[array[i]] += 1;
-	for (i = 0; i < (max + 1); i++)
-		count[i] += count[i - 1];
-	print_array(count, max + 1);
+		numb[array[i]] += 1;
+	for (i = 0; i < (maximum + 1); i++)
+		numb[i] += numb[i - 1];
+	print_array(numb, maximum + 1);
 
 	for (i = 0; i < (int)size; i++)
 	{
-		sorted[count[array[i]] - 1] = array[i];
-		count[array[i]] -= 1;
+		arranged[numb[array[i]] - 1] = array[i];
+		numb[array[i]] -= 1;
 	}
 
 	for (i = 0; i < (int)size; i++)
-		array[i] = sorted[i];
+		array[i] = arranged[i];
 
-	free(sorted);
-	free(count);
+	free(arranged);
+	free(numb);
 }
